7f5a83e0af00efb80db0f19c3778085ed297dca2
harshithlaxman <harshu8310@gmail.com>
2021-07-03 02:27:04 +0530

    Hardhat configs

--- contracts/TokenERC.sol
+++ contracts/TokenERC.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import "../node_modules/@openzepelin/contracts/accessOwnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 import "./Auth.sol";
 contract Box is Ownable {
